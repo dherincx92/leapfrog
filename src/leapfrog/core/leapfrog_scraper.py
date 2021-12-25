d8943a6b01ab819ca826dcab5bccfd94ff8a08ea
Derek Herincx <derek663@gmail.com>
2021-12-25 13:09:37 -0500

    Adding webdriver wait to ensure results are loaded before scraping; minor restructuring

--- src/leapfrog/core/leapfrog_scraper.py
+++ src/leapfrog/core/leapfrog_scraper.py
@@ -1,11 +1,16 @@
 '''
 Scraper for https://www.hospitalsafetygrade.org/
 
+# TODO: add validation to ensure driver is closed
+
 author(s): Derek Herincx, derek663@gmail.com
 last_updated: 12/24/2021
 '''
 from typing import List, Dict
-from selenium.webdriver.remote.webelement import WebElement
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 from utilities import Driver
 
@@ -18,13 +23,16 @@ class LeapfrogScraper(Driver):
 
     Args:
         pagination: bool
-            Does the results page have multiple page results?
+            Does the results page have multiple pages of results?
         class_name: str
             Classs name of HTML elements containing relevant data
+        timeout: int
+            Number of seconds before driver times out
     """
     def __init__(self, pagination=False, **kwargs):
         self.pagination = False
         self.class_name = "leapfrogSearchResult"
+        self.timeout = 20 # seconds
         super().__init__(**kwargs)
 
     @property
@@ -42,11 +50,23 @@ class LeapfrogScraper(Driver):
                 Metadata as a list of Python dictionaries
         """
         driver = self.driver
-        tags = driver.find_elements_by_class_name(self.class_name)
-        data = []
-        for tag in tags:
-            # note: return statement ensures data is returned to Python env.
-            metadata = driver.execute_script("return arguments[0].dataset", tag)
-            data.append(metadata)
+        css_selector = (By.CSS_SELECTOR, f".resultsList > .{self.class_name}")
 
-        return data
+        try:
+            # sometimes, webpage is slow and results are still loading; this
+            # ensures we don't begin the scraping before the results load
+            element = WebDriverWait(driver, self.timeout).until(
+                EC.presence_of_element_located(css_selector)
+            )
+
+            tags = driver.find_elements_by_class_name(self.class_name)
+
+            data = []
+            for tag in tags:
+                # note: return statement ensures data is returned to Python env.
+                metadata = driver.execute_script("return arguments[0].dataset", tag)
+                data.append(metadata)
+
+            return data
+        finally:
+            driver.close()
